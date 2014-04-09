1ac15ac0bc68faa10be99e613a1a35071b9de666
Dan Jesensky <frandstorage@gmail.com>
2014-04-09 14:02:19 -0400

    Unnecessary comment.

--- Common/src/main/java/me/jesensky/dan/playertracker/TimeUtils.java
+++ Common/src/main/java/me/jesensky/dan/playertracker/TimeUtils.java
@@ -3,9 +3,6 @@ package me.jesensky.dan.playertracker;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-/**
- * Created by 14jesenskyd on 4/9/2014.
- */
 public final class TimeUtils {
     public static String getTime() {
         return getTime("dd MM yyyy");
