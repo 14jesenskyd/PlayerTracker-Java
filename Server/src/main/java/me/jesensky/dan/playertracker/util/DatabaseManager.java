bed44df5bd307088712fffeac1dbad65fc0cb962
Dan Jesensky <frandstorage@gmail.com>
2014-04-11 14:20:52 -0400

    Added initial mysql code for serverside

--- Server/src/main/java/me/jesensky/dan/playertracker/util/DatabaseManager.java
+++ Server/src/main/java/me/jesensky/dan/playertracker/util/DatabaseManager.java
@@ -1,5 +1,8 @@
 package me.jesensky.dan.playertracker.util;
 
+import com.mysql.jdbc.MySQLConnection;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,18 +14,26 @@ public class DatabaseManager {
     private String username;
     private String password;
     private String table;
+    private String db;
     private int port;
 
-    public DatabaseManager(String host, int port, String user, String password, String table, String defaults) throws SQLException {
+    public DatabaseManager(String host, int port, String user, String password, String table, String db, String defaults) throws SQLException {
         this.host = host;
         this.port = port;
         this.username = user;
         this.password = password;
         this.table = table;
+        this.db = db;
         this.connect(defaults);
     }
 
     private void connect(String defaults){
+        MysqlDataSource d = new MysqlDataSource();
+        d.setUser(this.username);
+        d.setPassword(this.password);
+        d.setServerName(this.host);
+        d.setPort(this.port);
+        d.setDatabaseName(this.db);
     }
 
     private void connect(){
