ee6b22280ba1035d221a4a4b0023e8b92570686c
Dan Jesensky <frandstorage@gmail.com>
2014-04-17 13:04:25 -0400

    Complete Connection refactor; add a stub to fetch data resposne

--- Server/src/main/java/me/jesensky/dan/playertracker/DataManager.java
+++ Server/src/main/java/me/jesensky/dan/playertracker/DataManager.java
@@ -3,9 +3,8 @@ package me.jesensky.dan.playertracker;
 import me.jesensky.dan.playertracker.exceptions.InvalidPacketException;
 import me.jesensky.dan.playertracker.listeners.DataTransmissionListener;
 import me.jesensky.dan.playertracker.net.Connection;
-import me.jesensky.dan.playertracker.net.packets.LoginResponsePacket;
-import me.jesensky.dan.playertracker.net.packets.Packet;
-import me.jesensky.dan.playertracker.net.packets.PacketType;
+import me.jesensky.dan.playertracker.net.NetUtils;
+import me.jesensky.dan.playertracker.net.packets.*;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -32,7 +31,7 @@ public class DataManager extends Thread{
 
     private boolean attemptLogin(String user, String pass){
         //TODO verify credentials
-        //Server.getSingleton().getDbManager();
+        Server.getSingleton().getDbManager();
         return false;
     }
 
@@ -40,22 +39,25 @@ public class DataManager extends Thread{
     public void run(){
         for(Connection c : Server.getSingleton().getConnectionManager().getConnections().values()){
             try {
-                if (c instanceof ClientConnection) {
                     if (c.dataRemaining()) {
                         Packet p = c.readData();
                         Packet response = null;
 
                         switch(p.getType()){
                             case LOGIN:
-
                                 response = new LoginResponsePacket((byte)3);
+                                break;
+                            case FETCH_DATA:
+                                FetchPacket packet = (FetchPacket)p;
+                                String name, notes, violations;
+                                UserViolationLevel vl;
+                                response = new DataResponsePacket();
                                 break;
                             default:
                                 continue;
                         }
                         response.sendData(c);
                     }
-                }
             }catch(IOException | InvalidPacketException e){
                 //TODO log exception
             }
