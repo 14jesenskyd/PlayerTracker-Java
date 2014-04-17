db44e4964c779dd1a50b66ed792c42120662116a
Dan Jesensky <frandstorage@gmail.com>
2014-04-17 13:58:16 -0400

    Unnecessary if only one Connection implemenation exists now

--- Server/src/main/java/me/jesensky/dan/playertracker/observers/DataTransmissionObserver.java
+++ Server/src/main/java/me/jesensky/dan/playertracker/observers/DataTransmissionObserver.java
@@ -1,15 +1,11 @@
 package me.jesensky.dan.playertracker.observers;
 
-import me.jesensky.dan.playertracker.ClientConnection;
 import me.jesensky.dan.playertracker.events.DataTransmissionEvent;
 import me.jesensky.dan.playertracker.listeners.DataTransmissionListener;
 
 public class DataTransmissionObserver implements DataTransmissionListener{
     @Override
     public void dataReceived(DataTransmissionEvent evt) {
-        if(!(evt.getConnection() instanceof ClientConnection))
-            return;
-
     }
 
     @Override
