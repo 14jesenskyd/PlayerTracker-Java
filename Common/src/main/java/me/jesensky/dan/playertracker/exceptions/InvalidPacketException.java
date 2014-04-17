fc085f45130942ee178fa89d1432e37c97320a9a
Dan Jesensky <frandstorage@gmail.com>
2014-04-17 13:41:52 -0400

    Add documentation for InvalidPacketException

--- Common/src/main/java/me/jesensky/dan/playertracker/exceptions/InvalidPacketException.java
+++ Common/src/main/java/me/jesensky/dan/playertracker/exceptions/InvalidPacketException.java
@@ -1,5 +1,9 @@
 package me.jesensky.dan.playertracker.exceptions;
 
+/**
+ * Represents when a packet contains invalid data,
+ * or could not be constructed correctly.
+ */
 public class InvalidPacketException extends Exception {
     public InvalidPacketException() {
         super("Invalid packet! Perhaps the header is malformed, or the byte data does not constitute a packet of that type?");
