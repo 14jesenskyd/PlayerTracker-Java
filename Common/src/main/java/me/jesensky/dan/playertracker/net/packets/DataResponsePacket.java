9360bfe8c5b1188f7ee3de59e4d51d6961e26756
Dan Jesensky <frandstorage@gmail.com>
2014-04-17 14:00:39 -0400

    Add some documentation for DataResponsePacket

--- Common/src/main/java/me/jesensky/dan/playertracker/net/packets/DataResponsePacket.java
+++ Common/src/main/java/me/jesensky/dan/playertracker/net/packets/DataResponsePacket.java
@@ -5,6 +5,28 @@ import me.jesensky.dan.playertracker.exceptions.InvalidArgumentException;
 import me.jesensky.dan.playertracker.exceptions.InvalidPacketException;
 import me.jesensky.dan.playertracker.net.NetUtils;
 
+/**
+ * Represents a response to a data fetch packet.<br />
+ * <br />
+ * Contains, in specific order:
+ * <ol>
+ *     <li>Player name, with exact capitalization of the
+ *     name stored in the database,</li>
+ *     <li>Notes, if any,</li>
+ *     <li>Infractions, if any; and finally,</li>
+ *     <li>The user's violation level.</li>
+ * </ol>
+ * <br />
+ * For easier access, one may use methods to access these
+ * without using
+ * {@link me.jesensky.dan.playertracker.net.packets.Packet}'s
+ * members.
+ *
+ * @see #getName()
+ * @see #getNotes()
+ * @see #getViolations()
+ * @see #getViolationLevel()
+ */
 public class DataResponsePacket extends Packet{
     public DataResponsePacket(byte... data) throws InvalidPacketException{
         super(PacketType.DATA_RESPONSE, data);
